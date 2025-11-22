4f258aa3bc06688918d600b57c87d3a4ca165ad8
f.iskandarov <f.iskandarov@hlsys>
2025-11-22 20:41:36 +0500

    chore: fix output file name and ensure banner is properly configured in Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,17 @@
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import banner from './build/banner.js';   // <-- THIS WAS MISSING
+import banner from './build/banner.js';
 
 export default {
     input: 'src/index.js',
     output: {
-        file: 'dist/grafana-logs.user.js',
+        file: 'dist/grafana-log-parser.user.js',
         format: 'iife',
-        banner,           // <-- NOW banner is defined
+        banner,
     },
     plugins: [
-        json(),           // allow package.json import
+        json(),
         resolve(),
         commonjs()
     ]
